9f7657551759e3de7e3478f11ce3fd286eabef40
toporkov.michael <toporkov.michael@gmail.com>
2018-11-13 13:31:40 +0300

    Fix poetry install

--- bin/install_requirements.sh
+++ bin/install_requirements.sh
@@ -4,6 +4,6 @@ if [ -f requirements.txt ]; then
     pip install --no-cache-dir --disable-pip-version-check -r requirements.txt
 elif [ -f setup.py ]; then
     pip install --no-cache-dir --disable-pip-version-check .
-elif [ -f pyproject.toml]; then
-    poetry install --no-dev --no-interaction
+elif [ -f pyproject.toml ]; then
+    source $HOME/.poetry/env && poetry install --no-dev --no-interaction
 fi
